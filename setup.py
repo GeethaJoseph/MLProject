162b083c31ac5b376b0abfc7aac130bed4f9dfca
Geetha <geehasjoseph@gmail.com>
2023-10-25 08:17:41 +0530

    setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+name='MLProject',
+version='0.0.1',
+author='Geetha',
+author_email ='geethasjoseph@gmail.com',
+packages= find_packages(),
+install_requires = ['pandas', 'numpy', 'seaborn']
+
+)
